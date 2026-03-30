708ee0d1d0efa9b5cd7fa19cbfde7cec953fb897
00karby-maker <00karby@gmail.com>
2026-03-30 16:54:51 +0900

    Update stats_page.dart

--- lib/pages/stats_page.dart
+++ lib/pages/stats_page.dart
@@ -13,14 +13,73 @@ class StatsPage extends StatefulWidget {
 class _StatsPageState extends State<StatsPage> {
   String format = "∀";
 
-  Color lrigColor(String name) {
-    final hash = name.hashCode;
-    final hue = ((hash * 137) % 360 + 360) % 360;
-    const saturation = 0.6;
-    const lightness = 0.5;
+  final List<String> lrigList = [
+// 白ルリグ
+  "タマ","タウィル","サシェ","リメンバ","ドーナ",
+  "アキノ","LION","ノヴァ","ゆかゆか","ガブリエラ",
+  "るう子","ゆきめ","エマ","にじさんじ","リゼ",
+  "アンジュ","アズサ","サオリ","ネージュ",
+// 赤ルリグ
+  "花代","ユヅキ","赤タマ","ララ・ルー","リル",
+  "カーニバル","レイラ","LOV","ヒラナ","LOVIT",
+  "エクス","アザエラ","ちより","ジール",
+// 青ルリグ
+  "ピルルク","エルドラ","ミルルン","ソウイ","あや",
+  "青リメンバ","青タマ","青ウムル","レイ","タマゴ",
+  "マドカ","みこみこ","ミカエラ","あきら","ネル",
+  "ミヤコ","リップル",
+// 緑ルリグ
+  "緑子","アン","アイヤイ","メル","ママ",
+  "緑ユヅキ","緑ピルルク","アト","WOLF","バン",
+  "サンガ","緑カーニバル","ひとえ","ホシノ","シロコ",
+  "ユカリ","ミーティア",
+// 黒ルリグ
+  "ウリス","イオナ","ウムル","ミュウ","ハナレ",
+  "アルフォウ","ナナシ","グズ子","黒カーニバル","ムジカ",
+  "デウス","マキナ","まほまほ","黒タマ","ヤミノ",
+  "ヒナ","シュン","とこ","ヴィオラ",
+// 無ルリグ
+  "夢限"
+];
 
-    return HSLColor.fromAHSL(1.0, hue.toDouble(), saturation, lightness).toColor();
-  }
+// 固定の色マップ
+final Map<String, Color> lrigColors = {
+    // 白ルリグ
+  "タマ": Color(0xFFffffff),"タウィル": Color(0xFFf8fbf8),"サシェ": Color(0xFFf3f3f2),"リメンバ": Color(0xFFc1e4e9),"ドーナ": Color(0xFFffdead),
+  "アキノ": Color(0xFFfef3c9),"LION": Color(0xFFfffae6),"ノヴァ": Color(0xFFfffaf0),"ゆかゆか": Color(0xFFffebcd),"ガブリエラ": Color(0xFFfaebd7),
+  "るう子": Color(0xFFe6e6e6),"ゆきめ": Color(0xFFdaf3ef),"エマ": Color(0xFFe5d2c5),"にじさんじ": Color(0xFFdcdddd),"リゼ": Color(0xFFcbb994),
+  "アンジュ": Color(0xFFe8ecef),"アズサ": Color(0xFFe5e4e6),"サオリ": Color(0xFFe9e4d4),"ネージュ": Color(0xFFeaf4fc),
+// 赤ルリグ
+  "花代": Color(0xFFe60033),"ユヅキ": Color(0xFFb15237),"赤タマ": Color(0xFFea5506),"ララ・ルー": Color(0xFFc2302a),"リル": Color(0xFFea5506),
+  "カーニバル": Color(0xFF752100),"レイラ": Color(0xFF8a3319),"LOV": Color(0xFFdb8449),"ヒラナ": Color(0xFFf6b894),"LOVIT": Color(0xFFeb6ea5),
+  "エクス": Color(0xFFce5242),"アザエラ": Color(0xFF683f36),"ちより": Color(0xFF872732),"ジール": Color(0xFF583133),
+// 青ルリグ
+  "ピルルク": Color(0xFF0095d9),"エルドラ": Color(0xFF674598),"ミルルン": Color(0xFF4c6cb3),"ソウイ": Color(0xFF274a78),"あや": Color(0xFF192f60),
+  "青リメンバ": Color(0xFF00a497),"青タマ": Color(0xFF7ebeab),"青ウムル": Color(0xFF00a381),"レイ": Color(0xFF7ebea5),"タマゴ": Color(0xFF9ba88d),
+  "マドカ": Color(0xFFc0c6c9),"みこみこ": Color(0xFF5383c3),"ミカエラ": Color(0xFFada250),"あきら": Color(0xFFbed2c3),"ネル": Color(0xFF84a2d4),
+  "ミヤコ": Color(0xFF5d7ea3),"リップル": Color(0xFF243d5c),
+// 緑ルリグ
+  "緑子": Color(0xFF3eb370),"アン": Color(0xFF028760),"アイヤイ": Color(0xFF88cb7f),"メル": Color(0xFF69b076),"ママ": Color(0xFF316745),
+  "緑ユヅキ": Color(0xFF00552e),"緑ピルルク": Color(0xFFdccb18),"アト": Color(0xFF98d98e),"WOLF": Color(0xFFe6eae3),"バン": Color(0xFF019a66),
+  "サンガ": Color(0xFFaab3a0),"緑カーニバル": Color(0xFF879a86),"ひとえ": Color(0xFF384e36),"ホシノ": Color(0xFFc2baab),"シロコ": Color(0xFFa2bd95),
+  "ユカリ": Color(0xFF436065),"ミーティア": Color(0xFF2b5f2a),
+// 黒ルリグ
+  "ウリス": Color(0xFF2b2b2b),"イオナ": Color(0xFF583822),"ウムル": Color(0xFF544a47),"ミュウ": Color(0xFF2e2930),"ハナレ": Color(0xFF0d0015),
+  "アルフォウ": Color(0xFF241a08),"ナナシ": Color(0xFF16160e),"グズ子": Color(0xFF250d00),"黒カーニバル": Color(0xFF302833),"ムジカ": Color(0xFF432f2f),
+  "デウス": Color(0xFF262626),"マキナ": Color(0xFF3e0014),"まほまほ": Color(0xFF24140e),"黒タマ": Color(0xFF595455),"ヤミノ": Color(0xFF524748),
+  "ヒナ": Color(0xFF797979),"シュン": Color(0xFFb7a49f),"とこ": Color(0xFF9fa0a0),"ヴィオラ": Color(0xFF80989b),
+// 無ルリグ
+  "夢限": Color(0xFFccffe5)
+};
+
+  Color lrigColor(String name) {
+  return lrigColors[name] ?? Colors.grey; // マップにない場合はグレー
+}
+
+  Color getTextColor(Color bgColor) {
+  final brightness = ThemeData.estimateBrightnessForColor(bgColor);
+  return brightness == Brightness.dark ? Colors.white : Colors.black;
+}
 
   /// データ取得
   List<MatchRecord> getRecords(Box<MatchRecord> box) {
@@ -132,12 +191,27 @@ class _StatsPageState extends State<StatsPage> {
                 PieChartData(
                   sections: data.entries.map((e) {
                     final percent = total == 0 ? 0.0 : (e.value / total * 100);
+                    final bgColor = lrigColor(e.key);
+                    final showLabel = percent >= 5;
                     return PieChartSectionData(
                       value: percent.toDouble(),
                       color: lrigColor(e.key),
                       radius: 65,
-                      title: "${e.key}\n${percent.toStringAsFixed(1)}%",
-                      titleStyle: const TextStyle(fontSize: 12, color: Colors.white),
+                      title: showLabel
+        ? "${e.key}\n${percent.toStringAsFixed(1)}%"
+        : "",
+                      titleStyle: TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+  color: getTextColor(bgColor),
+  shadows: [
+    Shadow(
+      blurRadius: 3,
+      color: Colors.black.withOpacity(0.7),
+      offset: Offset(1, 1),
+    ),
+  ],
+),
                     );
                   }).toList(),
                 ),
